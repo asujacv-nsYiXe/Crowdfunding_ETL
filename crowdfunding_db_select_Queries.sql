6c3a7f616baca673224764f401458f37330c8d9b
asujacv-nsXiYe <125230352+asujacv-nsYiXe@users.noreply.github.com>
2023-05-25 18:31:27 -0500

    Final

---
+++ crowdfunding_db_select_Queries.sql
@@ -0,0 +1,11 @@
+--1. Check category table
+SELECT * FROM category;
+
+--2. Check subcategory table
+SELECT * FROM subcategory; 
+
+--3. Check contacts table
+SELECT * FROM contacts; 
+	
+--4. Check campagin table
+SELECT * FROM campaign; 
